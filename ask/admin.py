89a1e1335a1c95c993bb990344da6545d205e6e4
Ondrej Sika <ondrej@ondrejsika.com>
2015-11-23 22:37:38 +0100

    pretify admin

--- ask/admin.py
+++ ask/admin.py
@@ -3,7 +3,38 @@ from django.contrib import admin
 from ask.models import Poll, Question, Answer, UserAnswer
 
 
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = (
+        'poll',
+        'question',
+        'allow_user_answer',
+    )
+    list_display_links = list_display
+    list_filter = list_display
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = (
+        'poll',
+        'question',
+        'answer',
+    )
+    list_display_links = list_display
+    list_filter = list_display
+
+
+class UserAnswerAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'poll',
+        'question',
+        'answer',
+    )
+    list_display_links = list_display
+    list_filter = list_display
+
+
 admin.site.register(Poll)
-admin.site.register(Question)
-admin.site.register(Answer)
-admin.site.register(UserAnswer)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
+admin.site.register(UserAnswer, UserAnswerAdmin)
